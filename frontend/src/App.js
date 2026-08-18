560077a83cf2c030b2122e89b658bf88b22d1293
Maneesha Dissanayake <87939128+maneeboy@users.noreply.github.com>
2026-08-18 10:38:40 +0530

    Post-advertisetement frontend page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 
 import Home from './Pages/Landing/Home'
 import Dashboard from './Pages/Seller/Dashboard'
+import PostAdvertesetment from './Pages/Seller/PostAdvertesetment';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/mainhome" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/post-advertisement" element={<PostAdvertesetment />} />
             </Routes>
         </React.Fragment>    
     </div>
