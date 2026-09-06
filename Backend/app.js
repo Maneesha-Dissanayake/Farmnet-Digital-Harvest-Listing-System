b2c954c0de5de9c480ea468e7db11e260e32664b
olithaDew <olithadewkalana@gmail.com>
2026-09-06 16:43:40 +0530

    fix errors

--- Backend/app.js
+++ Backend/app.js
@@ -88,8 +88,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '20mb'}));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 // Routes
 app.use("/api/chat", chatRoutes);
